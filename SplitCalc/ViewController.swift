2066b4f5ea994dba7269a392fd370d075db04f61
kurosawa <kuro6412@gmail.com>
2016-02-12 16:51:09 +0900

    pickerViewを追加したがどれを選んでも結果に反映されない

--- SplitCalc/ViewController.swift
+++ SplitCalc/ViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITextFieldDelegate {
+class ViewController: UIViewController,UITextFieldDelegate, UIPickerViewDelegate {
 
     @IBOutlet weak var totalMoney: UITextField!
     @IBOutlet weak var people: UITextField!
     @IBOutlet weak var answer: UILabel!
     @IBOutlet weak var shortage: UILabel!
-    
+    @IBOutlet weak var pickerVIew: UIPickerView!
+    var unitArray = ["1円","10円","100円","1000円"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -32,6 +33,7 @@ class ViewController: UIViewController,UITextFieldDelegate {
     @IBAction func calcButton(sender: AnyObject) {
         var x1 = 0
         var x2 = 0
+        var kirisage = 0
         if(totalMoney.text! != ""){
             x1 = Int(totalMoney.text!)!
         }else{
@@ -42,12 +44,60 @@ class ViewController: UIViewController,UITextFieldDelegate {
         }else{
             x2 = 1
         }
+        switch pickerVIew.selectedRowInComponent(0) {
+        case 0:
+            x1 / 1
+        case 1:
+            x1 / 10
+            kirisage = x1 % 10
+        case 2:
+            x1 / 100
+            kirisage = x1 % 100
+        case 3:
+            x1 / 1000
+            kirisage = x1 % 1000
+        default:
+            break
+        }
+        
         let tmpAns = x1 / x2
         let amari = x1 % x2
+        switch pickerVIew.selectedRowInComponent(0) {
+        case 0:
+            tmpAns*1
+            amari*1
+        case 1:
+            tmpAns*10
+            amari*10 + kirisage
+        case 2:
+            tmpAns*100
+            amari*100 + kirisage
+        case 3:
+            tmpAns*1000
+            amari*1000 + kirisage
+        default:
+            break
+        }
         answer.text = String(tmpAns)
         shortage.text = String(amari)
     }
+    
     @IBAction func tapScreen(sender: AnyObject) {
+        self.view.endEditing(true)
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        
+        return 1
+        
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        return unitArray.count;
+    }
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return unitArray[row]
     }
 }
 
