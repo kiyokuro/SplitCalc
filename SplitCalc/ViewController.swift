5498dc601e4486942ecf81c1435ae6047218f555
kurosawa <kuro6412@gmail.com>
2016-02-12 10:41:41 +0900

    Initial Commit

---
+++ SplitCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SplitCalc
+//
+//  Created by 黒澤清和 on 2016/02/12.
+//  Copyright © 2016年 黒澤清和. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
